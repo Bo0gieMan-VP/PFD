7579784695a40340993099678c9c7d929d56354f
Bo0gieMan-VP <74719564+Bo0gieMan-VP@users.noreply.github.com>
2021-12-27 22:48:05 +0200

    Update youtube.py

--- apis/youtube.py
+++ apis/youtube.py
@@ -6,7 +6,7 @@ import requests
 PINK_FLOYD_YT_ID = 'UCY2qt3dw2TQJxvBrDiYGHdQ'
 
 def play_song(songname):
-    results = requests.get(f'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId={PINK_FLOYD_YT_ID}&maxResults=3&q={songname}&key=AIzaSyAwBFTOeeKCpFAvzOJ3wzlJD7tIiG4zCF0').json()['items']
+    results = requests.get(f'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId={PINK_FLOYD_YT_ID}&maxResults=3&q={songname}&key=[ YOUR_API_KEY ]').json()['items']
     names = [result['snippet']['title'] for result in results]
     has_official = any('official' in name.lower() for name in names)
     for result in results:
